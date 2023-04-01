fb2aeb8c1513e6d82a8a1e6e40b529581f54f03b
Raphael Manke <github@manke.cloud>
2023-04-01 11:25:56 +0200

    feat(target): Add Sns target
    closes: #9

---
+++ src/targets/SnsTarget.ts
@@ -0,0 +1,21 @@
+import { IRole } from 'aws-cdk-lib/aws-iam';
+import { CfnPipe } from 'aws-cdk-lib/aws-pipes';
+import { ITopic } from 'aws-cdk-lib/aws-sns';
+import { IPipeTarget } from '../PipeTarget';
+
+export class SnsTarget implements IPipeTarget {
+  private topic: ITopic;
+  public readonly targetArn: string;
+  public readonly targetParameters: CfnPipe.PipeTargetParametersProperty;
+
+  constructor(topic: ITopic) {
+    this.topic = topic;
+    this.targetArn = topic.topicArn;
+    this.targetParameters = {
+    };
+  }
+
+  grantPush(grantee: IRole): void {
+    this.topic.grantPublish(grantee);
+  }
+}
